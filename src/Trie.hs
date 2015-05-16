1e753801e688397d2e1b34f3c0bcc26417b93bb0
Sönke Hahn <soenkehahn@gmail.com>
2015-05-16 17:59:39 +0800

    make Tries polymorph

--- src/Trie.hs
+++ src/Trie.hs
@@ -8,40 +8,39 @@ import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import           GHC.Generics
 
-data Trie
-  = Trie Bool (Map Char Trie)
+data Trie a
+  = Trie Bool (Map a (Trie a))
   deriving (Show, Generic)
 
-instance NFData Trie
+instance NFData a => NFData (Trie a)
 
-pp :: Trie -> String
+pp :: Trie a -> String
 pp = error "pp"
 
-empty :: Trie
+empty :: Trie a
 empty = Trie False M.empty
 
-insert :: String -> Trie -> Trie
-insert "" (Trie _ cs) = Trie True cs
+insert :: Ord a => [a] -> Trie a -> Trie a
 insert (a : r) (Trie b cs) = case M.lookup a cs of
   Nothing -> Trie b (M.insert a (insert r empty) cs)
   Just c -> Trie b (M.insert a (insert r c) cs)
+insert [] (Trie _ cs) = Trie True cs
 
-fromList :: [String] -> Trie
+fromList :: Ord a => [[a]] -> Trie a
 fromList = foldl' (flip insert) empty
 
-toList :: Trie -> [String]
+toList :: Trie a -> [[a]]
 toList (Trie b cs) =
-  (if b then [""] else []) ++
+  (if b then [[]] else []) ++
   concat (map (\ (c, t) -> map (c :) (toList t)) (M.toList cs))
 
-member :: String -> Trie -> Bool
+member :: Ord a => [a] -> Trie a -> Bool
 member [] (Trie b _) = b
 member (a : r) (Trie _ cs) = case M.lookup a cs of
   Nothing -> False
   Just c -> member r c
 
-delete :: String -> Trie -> Trie
-delete "" (Trie _ cs) = Trie False cs
+delete :: Ord a => [a] -> Trie a -> Trie a
 delete (a : r) (Trie b cs) = Trie b $ case M.lookup a cs of
   Nothing -> cs
   Just c ->
@@ -50,13 +49,14 @@ delete (a : r) (Trie b cs) = Trie b $ case M.lookup a cs of
       (Trie False ccs) | M.null ccs ->
         M.delete a cs
       _ -> M.insert a newChild cs
+delete [] (Trie _ cs) = Trie False cs
 
-union :: Trie -> Trie -> Trie
+union :: Ord a => Trie a -> Trie a -> Trie a
 union (Trie aIn aChildren) (Trie bIn bChildren) =
   Trie (aIn || bIn)
     (M.fromAscListWith union (mergeLists (M.toAscList aChildren) (M.toAscList bChildren)))
  where
-  mergeLists :: [(Char, Trie)] -> [(Char, Trie)] -> [(Char, Trie)]
+  mergeLists :: Ord a => [(a, Trie a)] -> [(a, Trie a)] -> [(a, Trie a)]
   mergeLists ((a, tA) : rA) ((b, tB) : rB) =
     if a <= b
       then (a, tA) : mergeLists rA ((b, tB) : rB)
--- test/TrieSpec.hs
+++ test/TrieSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module TrieSpec where
@@ -14,40 +15,40 @@ spec :: Spec
 spec = do
   describe "insert" $ do
     it "works" $ do
-      property $ \ list a ->
+      property $ \ list (a :: String) ->
         a `member` insert a (fromList list)
 
   describe "fromList" $ do
     it "complements toList" $ do
-      property $ \ list ->
+      property $ \ (list :: [String]) ->
         sort (toList (fromList list)) === sort (nub list)
 
   describe "delete" $ do
     it "works" $ do
-      property $ \ list ->
+      property $ \ (list :: [String]) ->
         not (null list) ==>
         forAll (elements list) $ \ e ->
         not (e `member` delete e (fromList list))
 
     it "leaves no useless branches in the trie" $ do
-      property $ \ list ->
+      property $ \ (list :: [String]) ->
         not (null list) ==>
         forAll (elements list) $ \ e ->
         not (hasDeadBranches (delete e (fromList list)))
 
   describe "union" $ do
     it "does the same as naiveUnion" $ do
-      property $ \ (fromList -> a) (fromList -> b) ->
+      property $ \ (fromList -> (a :: Trie Char)) (fromList -> b) ->
         toList (a `union` b)
           `shouldBe`
         toList (a `naiveUnion` b)
 
-hasDeadBranches :: Trie -> Bool
+hasDeadBranches :: Trie a -> Bool
 hasDeadBranches _root@(Trie _ cs) = any inner (M.elems cs)
   where
     inner (Trie isElem cs) =
       (null (M.toList cs) && not isElem) ||
       any hasDeadBranches (M.elems cs)
 
-naiveUnion :: Trie -> Trie -> Trie
+naiveUnion :: Ord a => Trie a -> Trie a -> Trie a
 naiveUnion a b = foldl' (flip insert) a (toList b)
